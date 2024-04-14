4a4a276eed5a6a71faf62ef21e3b3f015bf61abf
NaN <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-04-14 16:40:33 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,71 +1,111 @@
-function GetTableLength(Table)
-    local Count = 0
-    for i, v in pairs(Table) do
-        Count = Count + 1
-    end
-    return Count
+--[[
+    Table Reconstructor - Original by MakeSureDudeDies (MSDD). Modified by Dottik.
+
+    A simple lua program to make a lua table into valid, runnable, lua code.
+]]
+
+local function tL(t)
+	if type(t) ~= "table" then
+		return 0
+	end
+	local a = 0
+	for _, _ in pairs(t) do
+		a = a + 1
+	end
+	return a
 end
 
-local function ReconstructTableCore(Table, DoRandom, NameIfDefined, Time, IsInsideMainTable)
-    if type(Table) == "table" then
-        local IndexCount = GetTableLength(Table)
-        for i,v in pairs(Table) do
-            local TableIndexIteration = Table[i]
-            if type(TableIndexIteration) == "string" then
-                if i == IndexCount then
-                    print("\"" .. TableIndexIteration .. "\"") -- " then iteration then "
-                else
-                    print("\"" .. TableIndexIteration .. "\"" .. ",") -- " then iteration then "
-                end
-            elseif type(TableIndexIteration) == "number" and TableIndexIteration == math.huge then
-                if i == IndexCount then
-                    print("math.huge") -- gotta do it this way or output is inf
-                else
-                    print("math.huge" .. ",") -- gotta do it this way or output is inf
-                end
-            elseif type(TableIndexIteration) == "number" and tostring(TableIndexIteration) == "-nan" then -- Only way i could think of.
-                if i == IndexCount then
-                    print("0/0") -- gotta do it this way or output is inf
-                else
-                    print("0/0" .. ",") -- gotta do it this way or output is inf
-                end
-            elseif type(TableIndexIteration) == "number" then
-                if i == IndexCount then
-                    print(TableIndexIteration) -- gotta do it this way or output is inf
-                else
-                    print(TableIndexIteration .. ",") -- gotta do it this way or output is inf
-                end
-            elseif type(TableIndexIteration) == "boolean" then
-                if i == IndexCount then
-                    print(tostring(TableIndexIteration))
-                else
-                    print(tostring(TableIndexIteration) .. ",")
-                end
-            elseif type(TableIndexIteration) == "table" then
-                if i == IndexCount then
-                    print("Reconstructing tables is not yet supported.")
-                else -- always passes to this because cannot get the index of it.
-                    print("Reconstructing tables is not yet supported.,")
-                end
-            else
-                print("Unable to obtain the type of this iteration")
-            end
-        end
-        if IsInsideMainTable then
-            print("},")
-        else
-            print("}")
-        end
-    else
-        print("The given argument is not a table")
-    end
+local function tL_nested(t)
+	if type(t) ~= "table" then
+		return 0
+	end
+	local a = 0
+	for _, v in pairs(t) do
+		if type(v) == "table" then
+			a = a + tL_nested(v)
+		end
+		a = a + 1 -- Even if it was a table, we still count the table index itself as a value, not just its subvalues!
+	end
+	return a
 end
 
-function ReconstructTable(Table)
-    local IndexCount = GetTableLength(Table)
-    local TimeTable = os.date("*t", os.time())
-    print("Reconstructed at: " .. TimeTable.day .. "." .. TimeTable.month .. "." .. TimeTable.year .. " " .. TimeTable.hour .. ":" .. TimeTable.min .. " - Unix Time")
-    print("Indexes: " .. IndexCount) -- Number of indexes in said table
-    print("Reconstructed table:\n")
-    ReconstructTableCore(Table, true, nil, os.time(), false)
+local function reconstruct_table(t_)
+	if type(t_) ~= "table" then
+		return string.format("-- Given object is not a table, rather a %s. Cannot reconstruct.", type(t_))
+	end
+
+	local function inner__reconstruct_table(t, isChildTable, childDepth)
+		local tableConstruct = ""
+		if not isChildTable then
+			tableConstruct = "local t = {\n"
+		end
+
+		if childDepth > 30 then
+			tableConstruct = string.format("%s\n--Cannot Reconstruct, Too much nesting!\n", tableConstruct)
+			return tableConstruct
+		end
+
+		for idx, val in pairs(t) do
+			local idxType = type(val)
+			if idxType == "boolean" then
+				tableConstruct = string.format(
+					'%s%s["%s"] = %s',
+					tableConstruct,
+					string.rep("\t", childDepth),
+					tostring(idx),
+					val and "true" or "false"
+				)
+			elseif idxType == "function" or idxType == "number" or idxType == "string" then
+				local v = tostring(val)
+
+				if idxType == "number" then
+					if string.match(tostring(v), "nan") then
+						v = "0 / 0"
+					elseif string.match(tostring(v), "inf") then
+						v = "math.huge"
+					end
+				end
+
+				if idxType == "string" then
+					v = string.format('"%s"', v)
+				end
+
+				tableConstruct =
+					string.format('%s%s["%s"] = %s', tableConstruct, string.rep("\t", childDepth), tostring(idx), v)
+			elseif idxType == "table" then
+				local r = inner__reconstruct_table(val, true, childDepth + 1)
+				tableConstruct =
+					string.format('%s%s["%s"] = {\n%s', tableConstruct, string.rep("\t", childDepth), tostring(idx), r)
+			elseif idxType == "nil" then
+				tableConstruct =
+					string.format('%s%s["%s"] = nil', tableConstruct, string.rep("\t", childDepth), tostring(idx))
+			elseif idxType == "userdata" then
+				tableConstruct = string.format(
+					'%s%s["%s"] = "UserData. Cannot represent."',
+					string.rep("\t", childDepth),
+					tableConstruct,
+					tostring(idx)
+				)
+			end
+			tableConstruct = string.format("%s,\n", tableConstruct)
+		end
+		if isChildTable then
+			return string.format("%s%s}", tableConstruct, string.rep("\t", childDepth - 1))
+		else
+			return string.format("%s}\n", tableConstruct)
+		end
+	end
+	local welcomeMessage = [[
+-- Table reconstructed using table_reconstructor by usrDottik (Idea by MakeSureDudeDies)
+-- Reconstruction   began   @ %s - GMT 00:00
+-- Reconstruction completed @ %s - GMT 00:00
+-- Indexes Found inside of the Table (W/o  Nested Tables): %d
+--                                   (With Nested Tables): %d
+]]
+	local begin = tostring(os.date("!%Y-%m-%d %H:%M:%S"))
+	local reconstruction = inner__reconstruct_table(t_, false, 1)
+	local finish = tostring(os.date("!%Y-%m-%d %H:%M:%S"))
+	welcomeMessage = string.format(welcomeMessage, begin, finish, tL(t_), tL_nested(t_))
+
+	return string.format("%s%s", welcomeMessage, reconstruction)
 end
